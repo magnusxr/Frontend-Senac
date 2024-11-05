727bff54d95e004fe8bd662ba5c3bd645af5c830
Magnus Xavier <magnusxavierr@outlook.com>
2024-11-04 21:32:11 -0300

    aula 17

---
+++ Aula 17/exemplo 2/main.js
@@ -0,0 +1,44 @@
+document.write("Olá, Terra!")
+
+alert("Olá!")
+console.log("Olá, tudo bem?")
+
+// variáveis
+
+var my_str = "Name";
+var my_nmbr = 5;
+var my_bool = true;
+var my_array = [1, "string", true]
+
+var var1 = "Olá, pessoal!";
+alert(var1)
+
+var var2 = 123;
+alert(var2)
+console.log(var2)
+
+let var3 = "let = variável local"
+alert(var3)
+
+/* operadores aritméticos
+básicos: + - * /
+resto: %
+potenciação: **
+incremento e decremento: ++ --
+
+operadores relacionais
+básicos: < <= > >=
+igualdade: == !=
+identidade: === !==
+propriedade: in
+
+operadores lógicos
+and: &&
+or: ||
+*/
+
+document.write("Olá, Marte!");
+
+document.write("<h1>Eu sou um título!</h1>");
+
+document.write("<title>Título da página</title>");
